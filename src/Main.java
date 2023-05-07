d71293fbf03f1804951f81d544c148f21c31fd31
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-05-07 16:28:37 +0300

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        int[] arr = {25, 33, 2, 5, 46, 13, 22};
+        System.out.println("Before " + Arrays.toString(arr));
+        MergeSort.sort(arr);
+        System.out.println("After " + Arrays.toString(arr));
 
     }
 }
