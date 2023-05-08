0d05543b85cf388570bb6b6f3704c930c657438f
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-05-08 14:33:42 +0300

    First Commit

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -6,25 +6,27 @@ public class MergeSort {
     }
 
     private static void sort(int[] arr, int[] aux, int left, int right) {
-        if (left <= right) return;
-        int mid = (left + right) / 2;
+        if (right <= left) return;
+        int mid = left + (right-left) / 2;
         sort(arr, aux, left, mid);
         sort(arr, aux, mid + 1, right);
         merge(arr, aux, left, mid, right);
     }
 
     private static void merge(int[] arr, int[] aux, int left, int mid, int right) {
-        for (int k = left; k < right; k++) {
+        for (int k = left; k <= right; k++) {
             aux[k] = arr[k];
         }
 
         int i = left;
         int j = mid + 1;
         for (int k = left; k <= right; k++) {
-            if (i > mid) arr[k] = aux[j++];
-            else if (j > right) arr[k] = aux[i++];
-            else if (aux[j] < aux[i]) arr[k] = aux[j++];
-            else arr[k] = aux[i++];
+            if (i > mid)
+                arr[k] = aux[j++];
+            else if (j > right)
+                arr[k] = aux[i++];
+            else if (aux[i] <= aux[j]) arr[k] = aux[i++];
+            else arr[k] = aux[j++];
         }
     }
 }
