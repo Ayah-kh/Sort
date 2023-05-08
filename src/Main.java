b6572d5071cc777f0bcd77cfa3b0ea9baabefb09
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-05-08 17:03:33 +0300

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
 import java.util.Arrays;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Integer[] arr = {1,2,5,23,-1,-6,14};
+        Integer[] arr = {1, 2, 5, 23, -1, -6, 14};
         System.out.println("Before " + Arrays.toString(arr));
         MergeSort.sort(arr);
         System.out.println("After " + Arrays.toString(arr));
 
+        Student Mohammed = new Student(1, "Mohammed");
+        Student Ayah = new Student(2, "Ayah");
+        Student Ayah2 = new Student(15, "Ayah");
+        Student Yaseen = new Student(9, "Yaseen");
+        Student Rima = new Student(7, "Rima");
+
+        Student[] students = {Mohammed, Ayah, Ayah2, Yaseen, Rima};
+        System.out.println(Arrays.toString(students));
+        MergeSort.sort(students);
+        System.out.println(Arrays.toString(students));
     }
 }
